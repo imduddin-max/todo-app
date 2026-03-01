27e89f89fb6f4dc066f7c1424e8ef5bcbf701581
Md Shahab Uddin <i_md.uddin@innovaccer.com>
2026-03-01 13:41:31 +0530

    Day 8: Todo Model and database setup

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',
+        'USER': 'i_md.uddin',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
