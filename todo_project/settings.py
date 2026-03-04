236872d9fc0157db65fdf95e9d41220be1fdf89a
Md Shahab Uddin <i_md.uddin@innovaccer.com>
2026-03-04 13:34:29 +0530

    Add Swagger documentation using drf-spectacular

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'todos',
 ]
 
@@ -125,3 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Todo App API',
+    'DESCRIPTION': 'A simple Todo API built with Django REST Framework',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
